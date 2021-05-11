0f6c44f5612840c8fe60a18d5e37960fc851cb75
yurii-kolodiir1 <77201680+yurii-kolodiir1@users.noreply.github.com>
2021-05-12 00:16:17 +0300

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      'env',
+      {
+        targets: {
+          node: '12.14',
+        },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-env',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    'react-hot-loader/babel',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          app: './src',
+        },
+      },
+    ],
+  ],
+};
